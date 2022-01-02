13bff7cb351ac66274a8a6653a82c9739b2c3857
Seran Shanmugathas <Seran@live.no>
2022-01-02 22:31:48 +0100

    Changed name of the files

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import TrackList from '../TrackList/TrackList';
 
-class SearchBarResult extends React.Component {
+class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/* <!-- Add a TrackList component --> */}
+                <TrackList />
             </div>
         );
     }
 }
 
-export default SearchBarResult;
+export default SearchResults;
