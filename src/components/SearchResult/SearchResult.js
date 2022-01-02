dedd359d12f2dfcf3e18803e8fe675e94a54f2f5
Seran Shanmugathas <Seran@live.no>
2022-01-02 22:16:39 +0100

    Added styles and jsx

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchBarResult extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <!-- Add a TrackList component --> */}
+            </div>
+        );
+    }
+}
+
+export default SearchBarResult;
