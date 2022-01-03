f9ecaa17f858272258892549de9e871d26f6e32f
Seran Shanmugathas <Seran@live.no>
2022-01-03 02:00:15 +0100

    Added props passing to track object

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+        this.addTrack = this.addTrack.bind(this);
+    }
+
     render() {
         return (
             <div className="TrackList">
-                {/* <!-- You will add a map method that renders a set of Track components  --> */}
+                {
+                    this.props.tracks.map(track => {
+                        return <Track key={track.id} 
+                                      name={this.props.track.name} 
+                                      artist={this.props.track.artist} 
+                                      album={this.props.track.album} 
+                                      onAdd={this.props.onAdd} 
+                                      onRemove={this.props.onRemove}
+                                      isRemoval={this.props.isRemoval} />;
+                    })
+                }
             </div>
         );
     }
