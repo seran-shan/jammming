ff25fd3c90f3498b22936624e2e94dc1538bacf6
Seran Shanmugathas <Seran@live.no>
2022-01-03 14:55:37 +0100

    Changed redirect uri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 
 const clientID = '55a6e6e84e8547858179ce82db7c2e60';
-const redirectURI = 'https://spotify-jammming.surge.sh/';
+const redirectURI = 'https://jammmingbyseran.surge.sh/';
 
 let accessToken; 
 
